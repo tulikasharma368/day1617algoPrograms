f303491765fbeb6ed83eb1341ee6f61cd8554a4d
Tulikasharma <tulikasharma368@gmail.com>
2021-08-14 00:35:26 +0530

    Sort array using insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,48 @@
+package dataStructureProblems;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InsertionSort {
+
+	void process() {
+		String str = getFromFile("C:\\Users\\Tulika Sharma\\Desktop\\text.txt");
+		String[] arr = str.split("\\s");
+		sort(arr);
+		//System.out.println(ans);
+	}
+	
+	public void sort(String[] ans) {
+		for (int i = 0; i < ans.length-1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (ans[j].compareTo(ans[j - 1]) < 0) {
+                    String temp = ans[j];
+                    ans[j] = ans[j - 1];
+                    ans[j - 1] = temp;
+                }
+            }
+        }
+		for(String i:ans) {
+			System.out.print(i+"\t");
+		}
+		 
+	}
+		
+	public static String getFromFile(String path) {
+		BufferedReader bufferReader = null;
+		String str = "";
+		try {
+			bufferReader = new BufferedReader(new FileReader(path));
+			str = bufferReader.readLine();
+			bufferReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return str;
+	}
+
+	public static void main(String[] args){
+		InsertionSort InsertionSort = new InsertionSort();
+		InsertionSort.process();
+	}
+}
